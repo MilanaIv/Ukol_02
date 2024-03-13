5119deda027ba0adf1b63167d26757f9031f260f
Milana Ivanova <milanaivanova92@gmail.com>
2024-03-13 16:45:59 +0100

    ff

--- script.js
+++ script.js
@@ -2,4 +2,11 @@
 
 Vytvořte webovou stránku, která požádá uživatele nejdříve o jeho křestní jméno a pak příjmení, obojí bez diakritiky. Pomocí metody trim zajistěte, že ve jméně ani příjmení nebudou na začátku ani na konci žádné bílé znaky. Pomocí metody slice vyřízněte ze jména i příjmení příslušné části. Pomocí interpolace řetězců sestavte výslednou adresu a vypište ji do stránky.*/
 
-document.body.innerHTML += 1
+const nameY = prompt('zadajte křestní jméno:');
+const surname = prompt('zadajte primeni:');
+
+const nameR = nameY.toLowerCase().replace("ě", "e").replace("č", "c").replace("ý", "y").replace("í", "i").replace("ř", "r").replace("ů", "u").replace("á", "a").replace("ž", "z");
+const surnameR = surname.toLowerCase().replace("ě", "e").replace("č", "c").replace("ý", "y").replace("í", "i").replace("ř", "r").replace("ů", "u").replace("á", "a").replace("ž", "z");
+
+
+document.body.innerHTML += `${nameR.slice(0, 3) + surnameR.slice(0, 5)}@fit.cvut.cz.`
